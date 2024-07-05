fc4636b4a92fac8aa930dfb512301e5e34fe1f90
Ishant Sahni <ishantsahni888@gmail.com>
2024-07-05 16:39:14 +0530

    mongo uri added

--- db.js
+++ db.js
@@ -1,14 +1,17 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 
-const mongoURI = 'mongodb+srv://ishantsahni888:Aventador#1@ecommercecluster.i7cswz3.mongodb.net/?retryWrites=true&w=majority&appName=eCommerceCluster';
+const mongoURI = 'mongodb+srv://ishant:Aventador%231@ishantcluster.fuipkbz.mongodb.net/?retryWrites=true&w=majority&appName=ishantCluster';
 
 const connectDB = async () => {
+    console.log("came here");
     try {
+        console.log("came here 2");
         await mongoose.connect(mongoURI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
+        console.log("came here")
         console.log('MongoDB connected successfully');
     } catch (err) {
         console.error('Error connecting to MongoDB:', err.message);

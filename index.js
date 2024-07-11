918904c95fb676a8243ecb52717d1b2745c5d691
Ishant Sahni <ishantsahni888@gmail.com>
2024-07-11 22:44:48 +0530

    file added

--- index.js
+++ index.js
@@ -3,23 +3,31 @@ const connectDB = require('./db');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const multer = require('multer');
+const path = require('path');
+const fs = require('fs');
 // const userModel = require('./models/userModel');
 const userRoutes = require('./routes/userRoutes');
 const userShoppingDetailsRoute = require('./routes/userShoppingDetailsRoutes');
 const UserShoppingDetailsModel = require('./models/userShoppingDetailsModel');
 require('dotenv').config();
 
-const allowedOrigins = ['http:localhost:3000']
+const allowedOrigins = ['http:localhost:3001']
 
 const app = express();
 
 // Connect to MongoDB
 connectDB();
 
+// Create the uploads directory if it doesn't exist
+const uploadDir = path.join(__dirname, 'uploads');
+if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir, { recursive: true }); // Ensure the directory is created recursively
+}
+
 // Define storage for multer
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, '/uploads');
+        cb(null, uploadDir);
     },
     filename: function (req, file, cb) {
         cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))

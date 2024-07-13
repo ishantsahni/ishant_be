d6123b7d838b248af0fdcc5012782166c29fb961
Ishant Sahni <ishantsahni888@gmail.com>
2024-07-13 12:46:53 +0530

    product details routes added

---
+++ routes/productDetailsRoutes/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const userModel = require("../../models/userModel");
+const userShoppingDetailsModel = require("../../models/userShoppingDetailsModel");
+const UserShoppingDetailsModel = require("../../models/userShoppingDetailsModel");
+
+const router = express.Router();
+
+router.get('/getProductDetails', async (req, res) => {
+    try {
+        const { name } = req.body;
+        const getUserDetails = await userModel.find({
+            name
+        });
+        const { email } = getUserDetails;
+        const getProductDetails = await UserShoppingDetailsModel.find({
+            email
+        });
+        console.log("product details ", getProductDetails);
+        res.status(200).json(getProductDetails);
+    } catch (error) {
+        console.log("Error while fetching product details info ", error);
+        res.status(500).send(error);
+    }
+
+})
+
+module.exports = router;

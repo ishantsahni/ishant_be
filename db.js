495208eb89b4fb3245a5d1ded6fe5381c6439360
Ishant Sahni <ishantsahni888@gmail.com>
2024-09-29 14:07:52 +0530

    mongo uri added to env file

--- db.js
+++ db.js
@@ -1,14 +1,11 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 
-const mongoURI = 'mongodb+srv://ishantsahni888:P5nPAqPC7rfhVMeE@ecommercecluster.asix2.mongodb.net/?retryWrites=true&w=majority&appName=eCommerceCluster';
-// const mongoURI = 'mongodb+srv://ishantsahni888:Aventador%1@ishantcluster.lxyvhdh.mongodb.net/?retryWrites=true&w=majority&appName=ishantCluster'
-
 const connectDB = async () => {
     console.log("came here");
     try {
         console.log("came here 2");
-        await mongoose.connect(mongoURI, {
+        await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });

c7f98499a6d5022371226800f6454239ff7a865f
Ishant Sahni <ishantsahni888@gmail.com>
2024-09-30 11:28:32 +0530

    db refactored

--- db.js
+++ db.js
@@ -2,14 +2,11 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-    console.log("came here");
     try {
-        console.log("came here 2");
         await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
-        console.log("came here")
         console.log('MongoDB connected successfully');
     } catch (err) {
         console.error('Error connecting to MongoDB:', err.message);

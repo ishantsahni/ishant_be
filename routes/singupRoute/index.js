b488d968880a826a3fd9f7c812a4a443b52e9f70
Ishant Sahni <ishantsahni888@gmail.com>
2024-10-13 12:55:29 +0530

    added the path for login

--- routes/singupRoute/index.js
+++ routes/singupRoute/index.js
@@ -57,4 +57,43 @@ router.post("/signup", async (req, res) => {
 
 })
 
+// User login
+router.post('/login', async (req, res) => {
+
+    const { email, password } = req.body;
+
+    try {
+        // Check if the user exists
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(500).json({ message: 'User does not exist!' });
+        }
+
+        // Compare password
+        const isMatch = await bycrypt.compare(password, user.password);
+        if (!isMatch) {
+            res.status(400).json({ message: 'Invalid password!' });
+        }
+
+        // Generate JWT token
+        const token = JWT.sign({ id: user._id }, process.env.JWT_SECRET_KEY, {
+            expiresIn: '1h'
+        })
+
+        res.status(200).json({
+            message: 'Login successful!',
+            token,
+            user: {
+                _id: user._id,
+                name: user.name,
+                email: user.email
+            }
+        })
+
+    } catch (error) {
+        res.status(500).json({ message: 'Server error!' });
+    }
+
+})
+
 module.exports = router;

780fe4fc3f9558066878d3347dd52e8db0d9fdc8
Ishant Sahni <ishantsahni888@gmail.com>
2024-10-31 11:29:56 +0530

    converted to post api

--- routes/productRoute/index.js
+++ routes/productRoute/index.js
@@ -4,63 +4,63 @@ const Product = require("../../models/Product");
 const router = express.Router();
 
 router.post("/add", async (req, res) => {
-    try {
-        const { name, description, price, category, brand, stock, image } = req.body;
-        const productData = {
-            name: name,
-            description: description,
-            price: price,
-            category: category,
-            brand: brand,
-            stock: stock,
-            image: image
-        }
-        const newProductData = new Product(productData);
-        const saveProductData = await newProductData.save();
-        if (saveProductData) {
-            res.status(200).send("Product Data saved successfully!");
-        }
-    } catch (error) {
-        res.status(500).send({
-            error: true,
-            message: error.message
-        })
+  try {
+    const { name, description, price, category, brand, stock, image } =
+      req.body;
+    const productData = {
+      name: name,
+      description: description,
+      price: price,
+      category: category,
+      brand: brand,
+      stock: stock,
+      image: image,
+    };
+    const newProductData = new Product(productData);
+    const saveProductData = await newProductData.save();
+    if (saveProductData) {
+      res.status(200).send("Product Data saved successfully!");
     }
+  } catch (error) {
+    res.status(500).send({
+      error: true,
+      message: error.message,
+    });
+  }
 });
 
-router.get("/get", async (req, res) => {
-    try {
-        const allProducts = await Product.find();
-        if (allProducts) {
-            res.status(200).send(allProducts);
-        }
-    } catch (error) {
-        res.status(500).send({
-            error: true,
-            message: error.message
-        })
+router.post("/getProducts", async (req, res) => {
+  try {
+    const allProducts = await Product.find();
+    if (allProducts) {
+      res.status(200).send(allProducts);
     }
-})
+  } catch (error) {
+    res.status(500).send({
+      error: true,
+      message: error.message,
+    });
+  }
+});
 
 router.get("/get/:id", async (req, res) => {
-    try {
-        const productId = req.params.id;
-        const product = await Product.findById({
-            _id: productId
-        });
+  try {
+    const productId = req.params.id;
+    const product = await Product.findById({
+      _id: productId,
+    });
 
-        if (product) {
-            res.status(200).send(product);
-        } else {
-            res.status(404).send("Product not found!");
-        }
-
-    } catch (error) {
-        res.status(500).send({
-            error: true,
-            message: error.message
-        })
+    if (product) {
+      res.status(200).send(product);
+    } else {
+      res.status(404).send("Product not found!");
     }
-})
+  } catch (error) {
+    res.status(500).send({
+      error: true,
+      message: error.message,
+    });
+  }
+});
 
 module.exports = router;

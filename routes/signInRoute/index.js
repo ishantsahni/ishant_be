05dc642fa5aa55003f2a8bf724b10356f31cbdcd
Ishant Sahni <ishantsahni888@gmail.com>
2024-11-10 14:24:59 +0530

    user _id updated to userId

--- routes/signInRoute/index.js
+++ routes/signInRoute/index.js
@@ -1,47 +1,48 @@
-const express = require('express');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const User = require("../../models/User");
 
 const router = express.Router();
 
 // User signin
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
+  const { email, password } = req.body;
 
-    const { email, password } = req.body;
-
-    try {
-        // Check if the user exists
-        const user = await User.findOne({ email });
-        if (!user) {
-            return res.status(500).json({ message: 'User does not exist!' });
-        }
-
-        // Compare password
-        const isMatch = await bcrypt.compare(password, user.password);
-        if (!isMatch) {
-            res.status(400).json({ message: 'Invalid password!' });
-        }
-
-        // Generate JWT token
-        const accessToken = jwt.sign({ id: user._id }, process.env.JWT_SECRET_KEY, {
-            expiresIn: '1h'
-        })
-
-        res.status(200).json({
-            message: 'Login successful!',
-            accessToken,
-            user: {
-                _id: user._id,
-                name: user.name,
-                email: user.email
-            }
-        })
-
-    } catch (error) {
-        res.status(500).json({ message: 'Server error!' });
+  try {
+    // Check if the user exists
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(500).json({ message: "User does not exist!" });
     }
 
-})
+    // Compare password
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      res.status(400).json({ message: "Invalid password!" });
+    }
+
+    // Generate JWT token
+    const accessToken = jwt.sign(
+      { userId: user._id },
+      process.env.JWT_SECRET_KEY,
+      {
+        expiresIn: "1h",
+      }
+    );
+
+    res.status(200).json({
+      message: "Login successful!",
+      accessToken,
+      user: {
+        userId: user._id,
+        name: user.name,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Server error!" });
+  }
+});
 
 module.exports = router;
--- routes/singupRoute/index.js
+++ routes/singupRoute/index.js
@@ -1,64 +1,67 @@
-const express = require('express');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const User = require("../../models/User");
-const { sendSignUpEmail } = require('../../utils/emailService');
+const { sendSignUpEmail } = require("../../utils/emailService");
 
 const router = express.Router();
 
 // User Registration(Sing-up)
 router.post("/", async (req, res) => {
-    const { name, email, password, address, city, postalCode, country } = req.body;
+  const { name, email, password, address, city, postalCode, country } =
+    req.body;
 
-    try {
-        // Check if user already exists
-        const existingUser = await User.findOne({ email });
-        if (existingUser) {
-            res.status(400).json({ message: 'User already exists!' });
-        }
-
-        // Hash the password
-        const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(password, salt);
-
-        // Create user
-        const user = new User({
-            name,
-            email,
-            password: hashedPassword,
-            shippingAddress: {
-                address,
-                city,
-                postalCode,
-                country
-            }
-        })
-
-        // Save user
-        await user.save();
-
-        // Send the sign up email
-        sendSignUpEmail(user.email, user.name);
-
-        // Generate JWT token
-        const accessToken = jwt.sign({ id: user._id }, process.env.JWT_SECRET_KEY, {
-            expiresIn: '1h'
-        })
-
-        res.status(201).json({
-            message: 'User registered successfully!',
-            accessToken,
-            user: {
-                _id: user._id,
-                name: user.name,
-                email: user.email
-            }
-        })
-
-    } catch (error) {
-        res.status(500).json({ message: 'Server error' });
+  try {
+    // Check if user already exists
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      res.status(400).json({ message: "User already exists!" });
     }
 
-})
+    // Hash the password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // Create user
+    const user = new User({
+      name,
+      email,
+      password: hashedPassword,
+      shippingAddress: {
+        address,
+        city,
+        postalCode,
+        country,
+      },
+    });
+
+    // Save user
+    await user.save();
+
+    // Send the sign up email
+    sendSignUpEmail(user.email, user.name);
+
+    // Generate JWT token
+    const accessToken = jwt.sign(
+      { userId: user._id },
+      process.env.JWT_SECRET_KEY,
+      {
+        expiresIn: "1h",
+      }
+    );
+
+    res.status(201).json({
+      message: "User registered successfully!",
+      accessToken,
+      user: {
+        userId: user._id,
+        name: user.name,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+});
 
 module.exports = router;

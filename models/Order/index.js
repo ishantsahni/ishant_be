1f2f1b578cfbe1f68fe056516de6133e7e575ad5
Ishant Sahni <ishantsahni888@gmail.com>
2024-11-11 23:55:03 +0530

    order schema updated

--- models/Order/index.js
+++ models/Order/index.js
@@ -1,52 +1,56 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const orderSchema = new Schema({
+const orderSchema = new Schema(
+  {
     user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: true
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
     },
     orderItems: [
-        {
-            product: {
-                type: mongoose.Schema.Types.ObjectId,
-                ref: 'Product',
-            },
-            quantity: { type: Number },
-            price: { type: Number }
-        }
+      {
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Product",
+        },
+        quantity: { type: Number },
+        price: { type: Number },
+      },
     ],
     shippingAddress: {
-        address: {
-            type: String,
-            required: true
-        },
-        city: {
-            type: String,
-            required: true
-        },
-        postalCode: {
-            type: String,
-            required: true
-        },
-        country: {
-            type: String,
-            required: true
-        }
+      address: {
+        type: String,
+        required: true,
+      },
+      city: {
+        type: String,
+        required: true,
+      },
+      postalCode: {
+        type: String,
+        required: true,
+      },
+      country: {
+        type: String,
+        required: true,
+      },
     },
     paymentMethod: {
-        type: String,
-        required: true
+      type: String,
+      default: "Online transaction",
+      required: true,
     },
     isPaid: {
-        type: Boolean,
-        default: false
+      type: Boolean,
+      default: true,
     },
     isDelivered: {
-        type: Boolean,
-        default: false
-    }
-}, { timestamps: true });
+      type: Boolean,
+      default: true,
+    },
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model('Order', orderScehma, 'orders')
+module.exports = mongoose.model("Order", orderSchema, "orders");
